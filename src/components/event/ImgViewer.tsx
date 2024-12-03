5e5a064db522adadb472a54585a46af7191656bb
andnd350309 <anhnd350309@gmail.com>
2024-12-03 16:52:46 +0700

    adjust image viewer

--- src/components/event/ImgViewer.tsx
+++ src/components/event/ImgViewer.tsx
@@ -1,3 +1,4 @@
+import { DownloadOutlined, ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons'
 import { Modal, Button } from 'antd'
 import { useState } from 'react'
 
@@ -19,9 +20,16 @@ export default function ImageViewer({
   extra,
 }: ImageViewerProps) {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [zoom, setZoom] = useState(1)
 
-  const handleOpen = () => setIsModalOpen(true)
-  const handleClose = () => setIsModalOpen(false)
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setZoom(1) // Reset zoom when modal is closed
+  }
+
+  const zoomIn = () => setZoom((prevZoom) => Math.min(prevZoom + 0.2, 3)) // Max zoom level
+  const zoomOut = () => setZoom((prevZoom) => Math.max(prevZoom - 0.2, 1)) // Min zoom level
 
   return (
     <>
@@ -31,7 +39,7 @@ export default function ImageViewer({
         alt={alt}
         width={width}
         height={height}
-        onClick={handleOpen}
+        onClick={openModal}
         className={`cursor-pointer ${className}`}
         style={{ objectFit: 'cover' }}
       />
@@ -39,7 +47,7 @@ export default function ImageViewer({
       {/* Fullscreen Modal */}
       <Modal
         open={isModalOpen}
-        onCancel={handleClose}
+        onCancel={closeModal}
         footer={null}
         centered
         width='80%'
@@ -48,21 +56,65 @@ export default function ImageViewer({
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'black',
+          backgroundColor: 'white',
           padding: '0',
         }}
+        closeIcon={<span style={{ fontSize: '18px', color: '#000000' }}>✕</span>}
         style={{ padding: '0' }}
         modalRender={(modal) => <div className='custom-modal-content'>{modal}</div>}
       >
+        <div
+          style={{
+            padding: '16px 24px',
+            borderBottom: '1px solid #f0f0f0',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '80%',
+          }}
+          className='ejehhhe'
+        >
+          <div>
+            <h2 style={{ margin: 0, fontSize: '16px', fontWeight: 'bold' }}>{alt}</h2>
+            <div style={{ color: '#888', fontSize: '14px' }}>
+              <span>Định dạng: </span>
+              {' | '}
+              <span>Dung lượng: </span>
+              {' | '}
+              <span>Kích thước: </span>
+            </div>
+          </div>
+          <Button type='primary' icon={<DownloadOutlined />} href={extra} download={alt}>
+            Tải về
+          </Button>
+        </div>
         <img
           src={extra}
           alt={alt}
-          style={{ maxWidth: '100%', maxHeight: '80vh', objectFit: 'contain' }}
+          style={{
+            transform: `scale(${zoom})`,
+            transition: 'transform 0.3s',
+            maxWidth: '100%',
+            maxHeight: '80vh',
+            objectFit: 'contain',
+          }}
         />
-        <div style={{ marginTop: '15px' }}>
-          <Button href={extra} download type='primary'>
-            Download
-          </Button>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '10px',
+            borderTop: '1px solid #f0f0f0',
+          }}
+        >
+          <Button
+            shape='circle'
+            icon={<ZoomOutOutlined />}
+            onClick={zoomOut}
+            disabled={zoom <= 1}
+            style={{ marginRight: '10px' }}
+          />
+          <Button shape='circle' icon={<ZoomInOutlined />} onClick={zoomIn} disabled={zoom >= 3} />
         </div>
       </Modal>
       <style jsx global>{`
