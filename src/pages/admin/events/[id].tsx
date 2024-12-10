6500de19e2cabc58a74b6b205c39fdeda04df10a
anhnd350309 <anhnd350309@gmail.com>
2024-12-10 21:42:19 +0700

    trigger

--- src/pages/admin/events/[id].tsx
+++ src/pages/admin/events/[id].tsx
@@ -3,7 +3,7 @@ import SEOHead from '@/components/seo'
 import LayoutAdmin from '@/components/layout/LayoutAdmin'
 import ListEventsDetailAdmin from '@/components/event/ListItemDetailAdmin'
 import { useRouter } from 'next/router'
-
+//
 const EventAdmin = () => {
   const router = useRouter()
   const { id } = router.query
