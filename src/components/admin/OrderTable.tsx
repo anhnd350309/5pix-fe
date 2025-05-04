3ad699f8a9582ce864fb244e9b44c5e08f5f0548
anhnd350309 <anhnd350309@gmail.com>
2025-05-04 21:15:09 +0700

    add detail order

--- src/components/admin/OrderTable.tsx
+++ src/components/admin/OrderTable.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { Table, Tag, Button, Tabs, Input, DatePicker, Space } from 'antd'
+import { Table, Tag, Button, Tabs, Input, DatePicker, Space, Spin } from 'antd'
 import { UserOutlined, MailOutlined, SearchOutlined } from '@ant-design/icons'
 import { listOrdersOrderListGet } from '@/services/order/order'
-import { CreateOrderResponse, GetMerchantsGetParams, ListOrdersOrderListGetParams } from '@/schemas'
+import {
+  CollectionImageWithQueryResponse,
+  CreateOrderResponse,
+  GetMerchantsGetParams,
+  ListOrdersOrderListGetParams,
+} from '@/schemas'
 import moment from 'moment'
 import { statusOrderMapping } from '@/constants/mapping'
 import useCurrency from '@/hooks/useCurrency'
+import { getImageCollectionCollectionItemGet } from '@/services/image-collection/image-collection'
 const { RangePicker } = DatePicker
 const { TabPane } = Tabs
 
@@ -38,8 +44,10 @@ const OrderTable: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [loadedOrders, setLoadedPOrders] = useState<CreateOrderResponse[]>([])
   const formatter = useCurrency('đ')
+  const [detailOrder, setDetailOrder] = useState<CollectionImageWithQueryResponse | null>(null)
   const [totalOrders, setTotalOrders] = useState<number | null>(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [isLoadingDetail, setIsLoadingDetail] = useState(true)
   const [error, setError] = useState<string | null>(null)
 
   const columns = [
@@ -88,8 +96,17 @@ const OrderTable: React.FC = () => {
     },
   ]
 
-  const expandedRowRender = (record: OrderData) => {
-    if (!record.details) return null
+  const expandedRowRender = (record: CreateOrderResponse) => {
+    if (isLoadingDetail) {
+      return <Spin />
+    }
+
+    if (!detailOrder) {
+      return <div>Không có thông tin chi tiết.</div>
+    }
+    console.log(detailOrder)
+    const imageGroups = detailOrder.image_queries ?? {}
+    const images = detailOrder.images ?? []
     return (
       <div className='p-6 bg-white shadow-md rounded-lg border border-gray-200 relative'>
         <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4'>
@@ -103,35 +120,90 @@ const OrderTable: React.FC = () => {
         <div className='grid grid-cols-1 sm:grid-cols-5 gap-4 sm:gap-20 mb-6'>
           <div className='flex flex-col space-y-4 col-span-2'>
             <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-2'>
-              <strong> Order ID:</strong> {record.orderId}
+              <strong> Order ID:</strong> {record.id}
             </p>
             <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-2'>
-              <strong>Order Creation Time:</strong> {record.details.orderCreationTime}
+              <strong>Order Creation Time:</strong> {record.created_at}
             </p>
             <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-2'>
-              <strong>Payment Time:</strong> {record.details.paymentTime}
+              <strong>Payment Time:</strong> {record.updated_at}
             </p>
             <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-2'>
-              <strong>Email:</strong> {record.details.email}
+              <strong>Email:</strong> {record.owner_email}
             </p>
           </div>
 
           <div className='flex flex-col space-y-4 col-span-3'>
             <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-5'>
               <strong className='col-span-2'>Order Status:</strong>
-              <Tag color='green' className='font-inter font-medium text-[14px] w-24 col-span-3'>
-                {record.status}
+              <Tag color={statusOrderMapping[record.internal_status ?? 'unknown']?.color}>
+                {statusOrderMapping[record.internal_status ?? 'unknown']?.text || 'Không xác định'}
               </Tag>
             </p>
-            <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-5'>
-              <strong className='col-span-2'>Album:</strong>{' '}
-              <div className='col-span-3'>{record.album}</div>
-            </p>
-            <p className='font-inter font-medium text-[14px] leading-[20px] text-gray-600 grid grid-cols-5'>
-              <strong className='col-span-2'>Note:</strong>{' '}
-              <div className='col-span-3'>{record.details.note}</div>
-            </p>
           </div>
+        </div>
+        <div className='space-y-4'>
+          {images.map((item, index) => (
+            <div
+              key={index}
+              className='flex items-center justify-between bg-white p-4 rounded-lg shadow-sm'
+            >
+              <div className='flex items-center'>
+                <img
+                  src={item.album_image_url}
+                  alt={item.album_image_name}
+                  className='w-12 h-12 rounded-lg object-cover mr-4'
+                />
+                <div>
+                  <div className='font-semibold'>{item.album_image_name}</div>
+                  <div className='text-sm text-gray-500 truncate w-[200px]'>Ảnh đơn</div>
+                </div>
+              </div>
+              <div className='font-semibold whitespace-nowrap'>
+                {formatter(record.line_items?.[0]?.album_image_price ?? 0)}
+              </div>
+            </div>
+          ))}
+          {Object.entries(imageGroups).map(([key, images]) => (
+            <div
+              key={key}
+              className='flex items-center justify-between bg-white p-4 rounded-lg shadow-sm'
+            >
+              <div>
+                <div className='font-semibold'>{images[0].album_name}</div>
+                <div className='text-sm text-gray-500 truncate w-[200px]'>
+                  {/* {image.album_image_name} */}
+                  Photobook của số bib {key}
+                </div>
+              </div>
+              <div className='font-semibold whitespace-nowrap'>
+                {formatter(record.line_items?.[0]?.album_price ?? 0)}
+              </div>
+              {/* {images.map((image) => (
+                <div
+                  key={image.id}
+                  className='flex items-center justify-between bg-white p-4 rounded-lg shadow-sm'
+                >
+                  <div className='flex items-center'>
+                    <img
+                      src={image.album_image_url}
+                      alt={image.album_image_name}
+                      className='w-12 h-12 rounded-lg object-cover mr-4'
+                    />
+                    <div>
+                      <div className='font-semibold'>{image.album_name}</div>
+                      <div className='text-sm text-gray-500 truncate w-[200px]'>
+                        {image.album_image_name}
+                      </div>
+                    </div>
+                  </div>
+                  <div className='font-semibold whitespace-nowrap'>
+                    {formatter(record.line_items?.[0]?.album_price ?? 0)}
+                  </div>
+                </div>
+              ))} */}
+            </div>
+          ))}
         </div>
       </div>
     )
@@ -168,6 +240,36 @@ const OrderTable: React.FC = () => {
   useEffect(() => {
     fetchOrders()
   }, [currentPage, activeTab])
+  useEffect(() => {
+    const fetchUserDetail = async () => {
+      if (!selectedRow) {
+        setDetailOrder(null)
+        return
+      }
+
+      setIsLoadingDetail(true)
+      setError(null)
+
+      try {
+        const response = await getImageCollectionCollectionItemGet({
+          collection_id: parseInt(selectedRow, 10),
+        })
+        const detail = response
+        if (detail) {
+          setDetailOrder(detail)
+        } else {
+          setDetailOrder(null)
+        }
+      } catch (err: any) {
+        setError(err.message || 'Something went wrong')
+      } finally {
+        setIsLoadingDetail(false)
+      }
+    }
+
+    fetchUserDetail()
+    console.log('Selected row:', selectedRow)
+  }, [selectedRow])
   return (
     <div className='container mx-auto p-4'>
       <Tabs defaultActiveKey='all' onChange={handleTabChange}>
@@ -193,17 +295,20 @@ const OrderTable: React.FC = () => {
       </div>
       <Table
         scroll={{ x: 'max-content' }}
+        rowKey={(record) =>
+          record.first_line_collection_id?.toString() ?? `temp-key-${Math.random()}`
+        }
         loading={isLoading}
         bordered
         columns={columns}
         dataSource={loadedOrders}
-        // expandable={{
-        //   expandedRowRender,
-        //   expandRowByClick: true,
-        //   onExpand: (expanded: boolean, record: OrderData) =>
-        //     setSelectedRow(expanded ? record.key : null),
-        //   expandedRowKeys: selectedRow ? [selectedRow] : [],
-        // }}
+        expandable={{
+          expandedRowRender,
+          expandRowByClick: true,
+          onExpand: (expanded: boolean, record: CreateOrderResponse) =>
+            setSelectedRow(expanded ? record.first_line_collection_id?.toString() || null : null),
+          expandedRowKeys: selectedRow ? [selectedRow] : [],
+        }}
         pagination={{
           current: currentPage,
           onChange: (page) => {
